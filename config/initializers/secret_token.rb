39f85afd800a42faf51926f139b7df00c4abdf64
Albert Jiao <albert.jiao@yale.edu>
2013-12-23 22:36:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlbertWebsite::Application.config.secret_token = '7869f14620f3521d1edee8bf82db2e951eabfb2473091dd1f69baa8e8ffe20e19dec19548bf70c72e0c7140789aa81be2e95d2fb098824576df4e50653dc4c5a'
